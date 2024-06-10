8e56ec8d104a206ea62526aea3ff6480bf899318
Nick Shi <nickjshi@outlook.com>
2024-06-09 23:08:10 -0700

    Done?

---
+++ CircularDoublyLinkedList/CircularDoublyLinkedList/Program.cs
@@ -0,0 +1,260 @@
+﻿
+namespace CircularDoublyLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CircularDoublyLinkedList<int> test = new CircularDoublyLinkedList<int>();
+             test.AddFirst(1);
+             test.AddLast(9);
+             test.AddBefore(test.Head, 3);
+             test.AddAfter(test.Tail, 5);
+             test.RemoveFirst();
+             test.RemoveLast();
+             test.Remove(test.Head.Value);
+
+            CircularDoublyLinkedList<int>.Node current = test.Head;
+            if (current != null)
+            {
+                do
+                {
+                    Console.WriteLine(current.Value);
+                    current = current.Next;
+                }
+                while (current != test.Head);
+            }
+            Console.WriteLine();
+
+            //     test.Remove(test.Tail.Value);
+             Console.WriteLine(test.IsEmpty());
+
+        }
+    }
+
+    public class CircularDoublyLinkedList<T>
+    {
+
+        public class Node
+        {
+
+            public T Value { get; set; }
+
+            public Node Next { get; set; }
+            public Node Previous { get; set; }
+
+            public Node(T value, Node next, Node previous)
+            {
+                Value = value;
+                Next = null;
+                Previous = null;
+            }
+
+            public Node(T Value)
+            {
+                this.Value = Value;
+                Next = this;
+                Previous = this;
+            }
+
+        }
+
+
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+        public int Count { get; set; }
+
+
+
+        public CircularDoublyLinkedList()
+        {
+            Head = Tail;
+            Tail = Head;
+           
+            Count = 0;
+        }
+
+        public void AddFirst(T value)
+        {
+            Node temp = new Node(value);
+            if (Head == null)
+            {
+                Head = temp;
+                Tail = Head;
+            }
+            else
+            {
+                Head.Previous = temp;
+                temp.Next = Head;
+                Head = temp;
+                Head.Previous = Tail;
+                Tail.Next = Head;
+                
+            }
+            Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            Node temp = new Node(value);
+            if (Head == null)
+            {
+                Head = temp;
+                Tail = Head;
+                Count++;
+                return;
+
+            }
+            Node last = Tail;
+            last.Next = temp;
+            temp.Previous = last;
+            Tail = temp;
+            Tail.Next = Head;
+            Head.Previous = Tail;
+            Count++;
+        }
+
+        public void AddBefore(Node node, T value)
+        {
+            Node temp = new Node(value);
+            Node current = Head;
+            if(node == null)
+            {
+                Count++;
+                return;
+            }
+            if (node == Head)
+            {
+                temp.Next = Head;
+                Head.Previous = temp;
+                Head = temp;
+                Head.Previous = Tail;
+                Tail.Next = Head;
+                Count++;
+                return;
+            }
+            
+            do
+            {
+                current = current.Next;
+            }
+            while (!current.Value.Equals(node.Value) && current!= Head);
+
+
+
+
+            if (current == node)
+            {
+                temp.Next = current;
+                temp.Previous = current.Previous;
+
+                current.Previous.Next = temp;
+                current.Previous = temp;
+            }
+            Count++;
+        }
+
+        public void AddAfter(Node node, T value)
+        {
+            Node temp = new Node(value);
+            Node current = Head;
+            do
+            {
+                current = current.Next;
+            }
+            while (!current.Value.Equals(node.Value) && current != Head);
+
+            if (current.Value.Equals(node.Value))
+            {
+                temp.Previous = current;
+                temp.Next = current.Next;
+
+                current.Next.Previous = temp;
+                current.Next = temp;
+
+            }
+
+            if(node == Tail)
+            {
+                Tail = temp;
+            }
+            Count++;
+        }
+
+        public bool RemoveFirst()
+        {
+            if (Head == null) return false;
+            else if (Head.Value.Equals(Tail.Value))
+            {
+                Head = null;
+                Tail = Head;
+                Count--;
+                return true;
+            }
+            Head = Head.Next;
+            Tail.Next = Head;
+            Count--;
+            return true;
+
+        }
+
+        public bool RemoveLast()
+        {
+            if (Tail == null) return false;
+            else if (Tail.Previous == Head)
+            {
+                Tail = null;
+                Head = Tail;
+                Count--;
+                return true;
+            }
+            Tail = Tail.Previous;
+            Tail.Next = Head;
+            Count--;
+            return true;
+        }
+
+        public bool Remove(T value)
+        {
+            if (value == null) return false;
+            Node current = Head;
+            if (value.Equals(Head.Value)) return RemoveFirst();
+            do
+            {
+                current = current.Next;
+            }
+            while (!current.Value.Equals(value) && current != Head);
+
+            if (value.Equals(Tail.Value))
+            {
+                Tail = Tail.Previous;
+                Tail.Next = Head;
+                Count--;
+                return true;
+            }
+
+            if (current.Value.Equals(value))
+            {
+                current.Previous = current.Next;
+                current.Next = current.Previous;
+                Count--;
+                return true;
+            }
+            Count--;
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Head == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+    }
+}
--- LinkedArrayShoppingList/LinkedArrayShoppingList/Item.cs
+++ LinkedArrayShoppingList/LinkedArrayShoppingList/Item.cs
@@ -6,7 +6,68 @@ using System.Threading.Tasks;
 
 namespace LinkedArrayShoppingList
 {
-    internal class Item
+    class Item
     {
+        private string name;
+        private double price;
+        private int quantity;
+
+        public Item()
+        {
+            name = "";
+            price = 0.0;
+            quantity = 0;
+        }
+
+        public Item(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public String toString()
+        {
+            return name + " (" + quantity + ") : $" + (price * quantity);
+        }
     }
 }
