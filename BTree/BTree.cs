d8a2bf3bac3c5c80c85745f76cd7147bf2393396
Nick Shi <nickjshi@outlook.com>
2025-04-13 00:42:59 -0700

    Fixed certain growth problems but realized that reset should occur on last growed node

--- BTree/BTree.cs
+++ BTree/BTree.cs
@@ -26,27 +26,18 @@ namespace BTreeProj
         private BTreeNode<T> SearchHelper(T key, BTreeNode<T> current)
         {
             BTreeNode<T> node = new BTreeNode<T>();
-
-            for (int i = 0; i < current.Keys.Count; i++)
+            int i = 0;
+            while (i < current.Keys.Count && key.CompareTo(current.Keys[i]) < 0)
             {
-                if (key.CompareTo(current.Keys[i]) < 0)
-                {
-                    if (current.Children[i] == null)
-                    {
-                        return current;
-                    }
-                    node = SearchHelper(key, current.Children[i]);
-                }
-                else if (i == current.Keys.Count - 1)
-                {
-                    if (current.Children[current.Children.Count - 1] == null)
-                    {
-                        return current;
-                    }
-                    node = SearchHelper(key, current.Children[current.Children.Count - 1]);
-                }
+                i++;
             }
-            return node;
+
+            if (i < current.Keys.Count && current.Keys[i].CompareTo(key) == 0) return current;
+
+            if (current.Children.Count == 0) return null;
+
+
+            return SearchHelper(key, current.Children[i]);
         }
 
         public void Add(T key)
@@ -58,57 +49,100 @@ namespace BTreeProj
                 return;
             }
             BTreeNode<T> SplitNode = SplitFinder(key, Root);
-            if (SplitNode != null)
-            {
+            while (SplitNode != null)
+            { 
+
                 if(!SplitNode.Equals(Root))
                 { 
-                     BTreeNode<T> current = Root;
+                    BTreeNode<T> current = Root;
                     int count = 0;
-                    while(!current.Children.Contains(SplitNode))
+                    while (!current.Children.Contains(SplitNode))
                     {
-                        if (SplitNode.Keys[0].CompareTo(current.Keys[count]) < 0)
+                        if (SplitNode.Keys[1].CompareTo(current.Keys[count]) < 0)
                         {
                             current = current.Children[count];
+                            count = 0;  
                         }
-                        else if (count == current.Keys.Count && SplitNode.Keys[count].CompareTo(current.Keys[count]) > 0)
+                        else if(SplitNode.Keys[1].CompareTo(current.Keys[count]) > 0 && current.Keys.Count == count + 1)
                         {
                             current = current.Children[count + 1];
-                        
+                            count = 0;
+
                         }
-                        count++;
+                        else
+                        {
+                            count++;
+                        }
+                        
                     }
+                    
                     current.Children.Remove(SplitNode);
-                    if (SplitNode.Keys[0].CompareTo(current.Keys[0]) < 0)
-                    {
-                        current.Keys.Insert(0, SplitNode.Keys[1]);
 
-                        current.Children.Insert(0, new BTreeNode<T>(SplitNode.Keys[0]));
-                        current.Children.Insert(1, new BTreeNode<T>(SplitNode.Keys[2]));
-                    }
-                    else
+                    int i = 0;
+                    while (SplitNode.Keys[1].CompareTo(current.Keys[i]) > 0)
                     {
-                        current.Keys.Insert(current.Keys.Count - 1, SplitNode.Keys[1]);
-
-                        current.Children.Insert(current.Children.Count - 1, new BTreeNode<T>(SplitNode.Keys[0]));
-                        current.Children.Insert(current.Children.Count, new BTreeNode<T>(SplitNode.Keys[2]));
+                        i++;
+                        if(i == current.Keys.Count)
+                        {
+                            
+                            break;
+                        }    
                     }
+                    current.Keys.Insert(i, SplitNode.Keys[1]);
+                    current.Children.Insert(i, new BTreeNode<T>(SplitNode.Keys[0]));
+                    
+                    current.Children.Insert(i + 1, new BTreeNode<T>(SplitNode.Keys[2]));
+                    if (SplitNode.Children.Count != 0)
+                    {
+                        List<BTreeNode<T>> tree = SplitNode.Children;
+                        for (int j = 0; j < tree.Count; j++)
+                        {
+                            if (j < 2)
+                            {
+                                Root.Children[i].Children.Insert(j, tree[j]);
+                            }
+                            else
+                            {
+                                Root.Children[i + 1].Children.Insert(j - 2, tree[j]);
+                            }
+                        }
+                    }
+
+                    SplitNode = SplitFinder(key, current);
                 }
                 else
                 {
                     Root = new BTreeNode<T>();
                     Root.Keys.Insert(0, SplitNode.Keys[1]);
 
+
                     Root.Children.Insert(0, new BTreeNode<T>(SplitNode.Keys[0]));
+
                     Root.Children.Insert(1, new BTreeNode<T>(SplitNode.Keys[2]));
+
+                    if (SplitNode.Children.Count != 0)
+                    {
+                        List<BTreeNode<T>> tree = SplitNode.Children;
+                        for(int i = 0; i < tree.Count; i++)
+                        {
+                            if(i < 2)
+                            {
+                                Root.Children[0].Children.Insert(i, tree[i]);
+                            }
+                            else
+                            {
+                                Root.Children[1].Children.Insert(i - 2, tree[i]);
+                            }
+                        }
+                    }
+                    SplitNode = SplitFinder(key, Root);
                 }
-               
+                
             }
             AddHelper(key, Root);
             Count++;
         }
-        
 
-        //should not need to check for size conditions since that should be solved when entering
         private BTreeNode<T> AddHelper(T key, BTreeNode<T> current)
         {
             BTreeNode<T> node = new BTreeNode<T>();
@@ -118,21 +152,23 @@ namespace BTreeProj
                 if (value < 0 && current.Children.Count != 0)
                 {
                     node = AddHelper(key, current.Children[i]);
+                    break;
                 }
                 else if (value > 0 && current.Children.Count != 0 && i == current.Keys.Count - 1)
                 {
                     node = AddHelper(key, current.Children[i + 1]);
+                    break;
                 }
                 else if (value < 0)
                 {
                     current.Keys.Insert(i, key);
-                    break;
+                    return current;
                 }
                 else if (value > 0 && current.Keys.Count < current.Keys.Capacity
                     && i == current.Keys.Count - 1)
                 {
                     current.Keys.Insert(i  + 1, key);
-                    break;
+                    return current;
                 }
             }
             return node;
@@ -149,10 +185,12 @@ namespace BTreeProj
                 if (value < 0 && i < current.Keys.Count && !isCountEqualCap)
                 {
                     node = SplitFinder(key, current.Children[i]);
+                    break;
                 }
                 else if(value > 0 && i == current.Keys.Count - 1 && !isCountEqualCap)
                 {
                     node = SplitFinder(key, current.Children[i + 1]);
+                    break;
                 }
                 else if (value < 0 && isCountEqualCap)
                 {
--- BTreeTester/BTreeTests.cs
+++ BTreeTester/BTreeTests.cs
@@ -7,17 +7,17 @@ namespace BTreeTester
         public void AddTest()
         {
             BTree<int> tree = new BTree<int>();
-            tree.Add(1);
-            tree.Add(2);
-            tree.Add(3);
-            tree.Add(0);
-            tree.Add(4);
-            tree.Add(5);
-            tree.Add(6);
-           // Assert.Contains(1, tree.Root.Keys);
-           // Assert.Contains(2, tree.Root.Keys);
-           // Assert.Contains(3, tree.Root.Keys);
-            
+            int[] arr = [50, 40, 60, 30, 70, 80, 20, 10, 90, 100,
+                 5, 110, 25, 35, 45, 55, 65, 75, 85, 95,
+                 105, 115, 120, 15, 1, 36, 46, 56, 66, 76];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                tree.Add(arr[i]);
+            }
+            Assert.Contains(50, tree.Root.Keys);
+            //Assert.Contains(20, tree.Root.Keys);
+            //Assert.Contains(6, tree.Root.Keys);
+
         }
 
         [Fact]
